35464966dd3a0bcd37d30932a1ea0542add75ef1
Oskar Persson <oskar.persson@polken.se>
2014-10-28 21:53:10 +0100

    Added hp to player

--- src/com/ioopm/Player.java
+++ src/com/ioopm/Player.java
@@ -6,5 +6,6 @@ public class Player extends Person {
     private Inventory inventory;
     private ArrayList<Course> finishedCourses;
     private ArrayList<Course> unfinishedCourses;
+    private int hp;
 
 }
