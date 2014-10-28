7ce5a3e4e19eaef685e47d70a14b9bd395315522
Oskar Persson <oskar.persson@polken.se>
2014-10-28 22:03:43 +0100

    Tabs -> Spaces in Main

--- src/com/ioopm/Main.java
+++ src/com/ioopm/Main.java
@@ -3,53 +3,53 @@ import java.util.Scanner;
 
 public class Main{
 
-	private Scanner scanner = new Scanner(System.in);
-	private String currentInput;
-	private Command command;
+    private Scanner scanner = new Scanner(System.in);
+    private String currentInput;
+    private Command command;
 
-	private enum Command {
-		GO, PICKUP, USEKEYWITH, DROP, INVENTORY,
-		ENROLL, TALK, GRADUATE, TRADE;
-	}
+    private enum Command {
+        GO, PICKUP, USEKEYWITH, DROP, INVENTORY,
+        ENROLL, TALK, GRADUATE, TRADE;
+    }
 
-	private Main(){
+    private Main(){
 
-	}
+    }
 
-	private void update(){
-		switch (command){
-		case GO:
-			System.out.println("go somewhere");
-			break;
-		default:
-			break;
-		}
-	}
+    private void update(){
+        switch (command){
+        case GO:
+            System.out.println("go somewhere");
+            break;
+        default:
+            break;
+        }
+    }
 
-	private void getInput(){
-		System.out.println("Enter command: ");
-		currentInput = scanner.nextLine().toLowerCase();
-		if (currentInput.substring(0, 3).equals("go ")){
-			String rest = currentInput.substring(3);
-			if (!(rest.equals("north") || rest.equals("south") || rest.equals("east") || rest.equals("west"))){
-				System.out.println("Incorrect input, try again");
-				getInput();
-			}else{
-				command = Command.GO;
-			}
-		}else{
-			System.out.println("Incorrect input, try again");
-			getInput();
-		}
-	}
+    private void getInput(){
+        System.out.println("Enter command: ");
+        currentInput = scanner.nextLine().toLowerCase();
+        if (currentInput.substring(0, 3).equals("go ")){
+            String rest = currentInput.substring(3);
+            if (!(rest.equals("north") || rest.equals("south") || rest.equals("east") || rest.equals("west"))){
+                System.out.println("Incorrect input, try again");
+                getInput();
+            }else{
+                command = Command.GO;
+            }
+        }else{
+            System.out.println("Incorrect input, try again");
+            getInput();
+        }
+    }
 
-	private void gameLoop(){
-		getInput();
-		update();
-	}
+    private void gameLoop(){
+        getInput();
+        update();
+    }
 
     public static void main(String[] args){
-		Main main = new Main();
+        Main main = new Main();
         World world = new World("res/world.txt");
         System.out.println(world);
         main.gameLoop();
