1be9763d883806ff71e5b83259f5529709767745
Oskar Persson <oskar.persson@polken.se>
2014-10-29 14:10:38 +0100

    Added key constructor

--- src/com/ioopm/Item.java
+++ src/com/ioopm/Item.java
@@ -2,4 +2,12 @@ package com.ioopm;
 
 public class Item {
     private int inventorySpace;
+
+    public Item(int space){
+        inventorySpace = space;
+    }
+
+    public int getSpace(){
+        return inventorySpace;
+    }
 }
--- src/com/ioopm/Key.java
+++ src/com/ioopm/Key.java
@@ -2,4 +2,8 @@ package com.ioopm;
 
 public class Key extends Item {
     private boolean used;
+
+    public Key(){
+        super(1);
+    }
 }
