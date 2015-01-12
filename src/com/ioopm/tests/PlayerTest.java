e2c0c199d7decff703a6733f3d2ad2dfa1b5804d
Oskar Persson <oskar.persson@polken.se>
2015-01-12 19:58:07 +0100

    More tests

--- src/com/ioopm/tests/PlayerTest.java
+++ src/com/ioopm/tests/PlayerTest.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class PlayerTest {
-    World world = new World("res/world.txt", "res/names.txt", "res/books.txt", "res/courses.txt", "res/questions.txt");
-    Room room = world.findRoom("Corridor 2");
+    World world = new World("res/testWorld.txt", "res/names.txt", "res/books.txt", "res/courses.txt", "res/questions.txt");
+    Room room = world.findRoom("Corridor 1");
     Course course = world.findCourse("IOOPM");
     Book book = new Book("Funktionellt, bitte!", "Tjark Weber", 1992);
     Teacher teacher = new Teacher("Tobias Wrigstad", course);
@@ -18,15 +18,40 @@ public class PlayerTest {
 
     @Test
     public void testGo() throws Exception {
+
         player.go("north");
-        assertEquals(player.getRoom(), world.findRoom("Room 5"));
+        System.out.println(player.getRoom());
+        assertEquals(player.getRoom(), world.findRoom("Room 2"));
+        player.go("south");
+        assertEquals(player.getRoom(), room);
+
+        player.go("east");
+        assertEquals(player.getRoom(), world.findRoom("Corridor 2"));
+        player.go("west");
+        assertEquals(player.getRoom(), room);
+
+        player.go("west");
+        assertEquals(player.getRoom(), world.findRoom("Room 1"));
+        player.go("east");
+        assertEquals(player.getRoom(), room);
+
+        player.go("south");
+        assertEquals(player.getRoom(), world.findRoom("Room 3"));
+        player.go("north");
+        assertEquals(player.getRoom(), room);
     }
 
     @Test
     public void testUnlock() throws Exception {
+        player.go("east");
         player.unlock("south");
         assertEquals(player.getRoom().getDoor("south").isLocked(), true);
 
+        player.getInventory().addItem(new Key());
+        player.unlock("south");
+        assertEquals(player.getRoom().getDoor("south").isLocked(), false);
+        player.go("south");
+        assertEquals(player.getRoom(), world.findRoom("Room 6"));
     }
 
     @Test
