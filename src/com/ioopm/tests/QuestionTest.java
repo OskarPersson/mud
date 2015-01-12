656b62bf5d54a2927dbfb3ad62b5d3bf5d42500b
Oskar Persson <oskar.persson@polken.se>
2015-01-12 18:00:31 +0100

    Added QuestionTest

---
+++ src/com/ioopm/tests/QuestionTest.java
@@ -0,0 +1,23 @@
+package com.ioopm.tests;
+
+import com.ioopm.Question;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class QuestionTest {
+
+    Question question = new Question("Question text", new String[]{"Answer #1", "Answer #2", "Answer #3"}, 2);
+
+    @Test
+    public void testGetCorrectAnswerIdx() throws Exception {
+        assertEquals(question.getCorrectAnswerIdx(), 2);
+    }
+
+    @Test
+    public void testAsk() throws Exception {
+        String text = question.ask(false);
+
+        assert(text.equals("Question text" + " (1-3)\n#1 " + "Answer #1" + "\n#2 " + "Answer #2" + "\n#3 " + "Answer #3" + "\n"));
+    }
+}
